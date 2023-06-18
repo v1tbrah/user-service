5e82a47795c1700ec99b9c1a44fd9a62739a0697
Viktor <khatibvictor97@gmail.com>
2023-06-18 21:43:55 +0400

    feat: Add latency metrics

--- internal/api/log.go
+++ internal/api/log.go
@@ -5,20 +5,28 @@ import (
 	"time"
 
 	"github.com/rs/zerolog/log"
+	"github.com/v1tbrah/promcli"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func interceptorLog(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+func (a *API) interceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	timeStart := time.Now()
-	defer func() {
-		duration := time.Since(timeStart)
-		if err != nil && status.Code(err) == codes.Internal {
-			log.Error().Msg(info.FullMethod + " in " + duration.String())
-			return
-		}
+
+	resp, err = handler(ctx, req)
+
+	duration := time.Since(timeStart)
+	statusCode := status.Code(err)
+	if err != nil && statusCode == codes.Internal {
+		log.Error().Msg(info.FullMethod + " in " + duration.String())
+		a.promCli.IncRequestResultCount(info.FullMethod, promcli.LabelError)
+	} else {
 		log.Info().Msg(info.FullMethod + " in " + duration.String())
-	}()
-	return handler(ctx, req)
+	}
+
+	a.promCli.IncRequestResultCount(info.FullMethod, promcli.LabelTotal)
+	a.promCli.ObserveRequestDurationSeconds(info.FullMethod, int(statusCode), duration)
+
+	return resp, err
 }
